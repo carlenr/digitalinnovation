de90cfca28328b50d3b3d4525f0577cc14445aeb
carlenr <carlosecsantos98@gmail.com>
2022-07-04 23:14:23 -0300

    Atualizado para aplicar o modificador private à função set da propriedade cpf.

--- main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,12 +1,13 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    val nome: String = "Carlos"
-    val cpf: String = "123.456.789-00"
+    var nome: String = "Carlos"
+    var cpf: String = "123.456.789-00"
+    private set
 }
 
 fun main(){
-    var carlos = Pessoa()
+    val carlos = Pessoa()
     println(carlos.nome)
     println(carlos.cpf)
 }
